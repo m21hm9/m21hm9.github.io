306b2a44663898238fc8deb5e7ae6c3d3f87d351
m21hm9 <matthewthom0629@gmail.com>
2026-03-16 23:54:13 +0800

    fix: update sever side from /graph to /knowledge-graph

--- app/graph/page.tsx
+++ app/graph/page.tsx
@@ -1,12 +1,5 @@
-"use client";
-
-import { KnowledgeGraph } from "@/components/knowledge-graph";
-import { DIGITAL_BRAIN_DATA } from "@/lib/knowledge-graph-data";
+import { redirect } from "next/navigation";
 
 export default function GraphPage() {
-  return (
-    <div className="h-screen w-screen bg-background">
-      <KnowledgeGraph data={DIGITAL_BRAIN_DATA} height={600} />
-    </div>
-  );
+  redirect("/knowledge-graph");
 }
